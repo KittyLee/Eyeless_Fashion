d7e57ac871d0054881b5c2fb0079614ce7fc6c65
KittyLee <katherine@codeforprogress.org>
2014-07-09 11:31:36 -0400

    modified admin and templates

--- eyeless/fashion/admin.py
+++ eyeless/fashion/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from fashion.models import EyelessUser, EyelessStylist, EyelessPost, EyelessMessage
 
 # Register your models here.
+admin.site.register(EyelessUser)
+admin.site.register(EyelessStylist)
+admin.site.register(EyelessPost)
+admin.site.register(EyelessMessage)
